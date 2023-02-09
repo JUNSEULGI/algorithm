28e66f50fd9aca254be960bb353c2a1e1e96b9f4
Jun Seulgi <us04185@gmail.com>
2023-02-09 22:59:18 +0900

    feat: fibonacci function dynamic programming(failed)

--- fibonacciFunction.py
+++ fibonacciFunction.py
@@ -1,23 +1,35 @@
 countZero, countOne = 0, 0
-czo = []
+czo = [[0, 0] for _ in range(41)]
 
 def fibonacci(n):
+    global countZero, countOne
     if n == 0:
-        global countZero
         countZero += 1
-        return 0
-    elif n == 1:
-        global countOne
+        return
+    if n == 1:
         countOne += 1
-        return 1
-    czo.append([countOne, countZero])
-    return fibonacci(n - 1) + fibonacci(n - 2)
+        return
+    if czo[n][0] > 0:
+        countZero += czo[n][0]
+        countOne += czo[n][1]
+        return
+    fibonacci(n - 2)
+    fibonacci(n - 1)
+    czo[n] = [countZero, countOne]
+    return
 
+# f(4)
+#     f(2)
+#         f(0) f(1)
+#         저장
+#     f(3)
+#         f(1) f(2)
+#         저장
+#     저장
 
 def countZeroAndOne(n):
     global countZero, countOne
     countZero, countOne = 0, 0
-
     fibonacci(n)
     print(countZero, countOne)
 
