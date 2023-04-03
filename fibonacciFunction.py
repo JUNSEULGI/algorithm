475f229db6c9174317ea632885ed71d0cb29a291
JUNSEULGI <us04185@gmail.com>
2023-04-03 19:25:36 +0900

    feat: fibonacci

--- fibonacciFunction.py
+++ fibonacciFunction.py
@@ -1,37 +1,24 @@
-countZero, countOne = 0, 0
 czo = [[0, 0] for _ in range(41)]
+czo[0] = [1,0]
+czo[1] = [0,1]
 
 def fibonacci(n):
-    global countZero, countOne
-    if n == 0:
-        countZero += 1
-        return
-    if n == 1:
-        countOne += 1
-        return
-    if czo[n][0] > 0:
-        countZero += czo[n][0]
-        countOne += czo[n][1]
-        return
-    fibonacci(n - 2)
-    fibonacci(n - 1)
-    czo[n] = [countZero, countOne]
-    return
-
-# f(4)
-#     f(2)
-#         f(0) f(1)
-#         저장
-#     f(3)
-#         f(1) f(2)
-#         저장
-#     저장
+    if czo[n][0] > 0 or czo[n][1] > 0:
+        return czo[n]
+    if czo[n-1][0] > 0 or czo[n-1][1] > 0:
+        czo_1 = czo[n-1]
+    else:
+        czo_1 = fibonacci(n - 1)
+    if czo[n-2][0] > 0 or czo[n-2][1] > 0:
+        czo_2 = czo[n-2]
+    else:
+        czo_2 = fibonacci(n - 2)
+    czo[n] = [czo_1[0] + czo_2[0], czo_1[1] + czo_2[1]]
+    return czo[n]
 
 def countZeroAndOne(n):
-    global countZero, countOne
-    countZero, countOne = 0, 0
     fibonacci(n)
-    print(countZero, countOne)
+    print(czo[n][0], czo[n][1])
 
 T = int(input())
 
